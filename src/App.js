4937d9a9751846a25b47a0d7e91fefda24da887e
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-17 15:14:35 -0600

    End of testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostContext";
-import Test from "./Test";
+// import Test from "./Test";
 
 function createRandomPost() {
   return {
@@ -134,7 +134,7 @@ function List() {
           </li>
         ))}
       </ul>
-      <Test />
+      {/* <Test /> */}
     </>
   );
 }
